e55efe5b74fa089860f55b97992fbae545da564e
CONCY <rony.concy@univ-rennes.fr>
2024-12-12 14:32:39 +0100

    implémentation du test pour le chargement du terrain

--- test/test_terrain.py
+++ test/test_terrain.py
@@ -7,8 +7,17 @@ from Terrain import Terrain, Case
 class TestTerrain(unittest.TestCase):
 
     def test_chargement(self):
-        # TODO
-        self.fail()
+        t = Terrain()
+        t.charger("terrains/t1.txt")
+
+        # Exemple attendu pour le contenu de `t1.txt`
+        self.assertEqual(t[9][17], Case.ENTREE)
+        self.assertEqual(t[7][6], Case.CLIENT)
+        self.assertEqual(t[4][17], Case.CLIENT)
+        self.assertEqual(t[2][10], Case.CLIENT)
+        self.assertEqual(t[1][0], Case.VIDE)
+        self.assertEqual(t[1][2], Case.OBSTACLE)
+        self.assertEqual(t[4][10], Case.OBSTACLE)
 
     def test_accesseur(self):
         t = Terrain()
